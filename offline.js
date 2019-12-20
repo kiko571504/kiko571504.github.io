d2a63a8129f6cfe9db598690b80c7f0bfbf1bcba
kiko571504 <47431386+kiko571504@users.noreply.github.com>
2019-12-20 15:26:38 +0300

    w

--- offline.js
+++ offline.js
@@ -1,12 +1,14 @@
 ﻿{
-	"version": 1576840017,
+	"version": 1576844766,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
-		"images/sprite-sheet0.png",
-		"images/sprite2-sheet0.png",
+		"images/menubutton-sheet0.png",
+		"images/menubutton-sheet1.png",
+		"images/modebutton-sheet0.png",
+		"images/scrollcenter-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
