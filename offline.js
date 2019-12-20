8f88642b15bdd0ea60c1e5aee7c9c89ce85eb27b
kiko571504 <47431386+kiko571504@users.noreply.github.com>
2019-12-20 14:09:28 +0300

    п

--- offline.js
+++ offline.js
@@ -1,16 +1,17 @@
 ﻿{
-	"version": 1576838207,
+	"version": 1576840017,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
-		"images/player-sheet0.png",
+		"images/sprite-sheet0.png",
 		"images/sprite2-sheet0.png",
-		"images/sprite4-sheet0.png",
-		"images/tiledbackground.png",
-		"images/stickback-sheet0.png",
-		"images/stick-sheet0.png",
-		"pathfind.js"
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
 	]
 }
